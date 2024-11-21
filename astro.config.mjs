e9559782ac6470490d564969c0446a41d9bebfaa
AdamVromman <adam.vromman@gmail.com>
2024-11-21 19:16:23 +0100

    install postcss and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   output: "hybrid",
   adapter: vercel(),
 });
