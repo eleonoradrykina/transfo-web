7c1fc26c7b308fc8c018fa5b4b655667db086aa1
AdamVromman <adam.vromman@gmail.com>
2024-11-24 18:51:47 +0100

    Add basic styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          DEFAULT: "#DEC1FB",
+        },
+        purple: {
+          DEFAULT: "#766DB8",
+        },
+        blue: {
+          DEFAULT: "#3F4194",
+          dark: "#222369",
+        },
+        orange: {
+          DEFAULT: "#EA5739",
+          dark: "#D8695E",
+          light: "#FA846C",
+        },
+        white: "#FBFBFB",
+        black: "#10113A",
+
+        gray: {
+          DEFAULT: "#E7E7E7",
+          dark: "#C8C8C8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
