39b90060f979d6eba49aef05e99d65e5b8e00be6
AdamVromman <adam.vromman@gmail.com>
2024-11-24 18:25:55 +0100

    add localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,15 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
+import { Language } from "./src/services/types";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "hybrid",
   adapter: vercel(),
+  i18n: {
+    defaultLocale: Language.NL,
+    locales: Array.from(Object.values(Language)),
+  },
 });
