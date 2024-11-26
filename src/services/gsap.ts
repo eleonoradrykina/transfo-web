11a47a2a0ed53fd741dafb5a4deb0b7dd401a235
AdamVromman <adam.vromman@gmail.com>
2024-11-26 22:20:03 +0100

    start schedule

---
+++ src/services/gsap.ts
@@ -0,0 +1,21 @@
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const mainTL = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".main",
+    start: "top top",
+    end: "+=500",
+    scrub: 1,
+    markers: {
+      startColor: "white",
+      endColor: "white",
+      fontSize: "18px",
+      fontWeight: "bold",
+      indent: 20,
+    },
+  },
+});
