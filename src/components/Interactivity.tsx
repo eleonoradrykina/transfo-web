fec9ab0ceacca570e83e6655e8a256a1ae044201
AdamVromman <adam.vromman@gmail.com>
2024-11-29 11:53:43 +0100

    add scrolling + first GSAP

---
+++ src/components/Interactivity.tsx
@@ -0,0 +1,12 @@
+import Map from "./Map";
+import Schedule from "./Schedule";
+
+const Interactivity = () => {
+  return (
+    <div className="interactivity">
+      <Map />
+      <Schedule />
+    </div>
+  );
+};
+export default Interactivity;
