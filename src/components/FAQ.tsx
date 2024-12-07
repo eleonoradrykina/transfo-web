4ac285d0b830e245b29542051049e5a5e8540e20
AdamVromman <adam.vromman@gmail.com>
2024-12-07 19:55:49 +0100

    change FAQ buttons to match design

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,12 +1,16 @@
 import Question from "./Question";
 
-const FAQ = () => {
+interface Props {
+  copy: any;
+}
+
+const FAQ = ({ copy }: Props) => {
   return (
     <div id="faq" className="faq">
       <button id="faq__back__button" className="faq__button back">
-        Terug naar overzicht
+        {copy.faq.back.toUpperCase()}
       </button>
-      <h2 className="faq__title">Veelgestelde vragen</h2>
+      <h2 className="faq__title"> {copy.faq.title}</h2>
       <div className="faq__questions">
         <Question question="Hoe geraak ik op het evenement?">
           <p>
